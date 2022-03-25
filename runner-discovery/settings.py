050b012922082e095504f17a055923bb6b833915
Shai <bianchishai@gmail.com>
2022-03-25 16:14:08 -0300

    Migrate runner-discovery to FastAPI

---
+++ runner-discovery/settings.py
@@ -0,0 +1,9 @@
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    port: int = Field(env='PORT', default=5000)
+    log_level_name: str = Field(env='LOG_LEVEL', default='INFO')
+
+
+settings = Settings()
